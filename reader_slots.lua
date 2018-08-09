07625880111f1c6ee9158434a2ee77067f248680
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-08-09 17:38:23 +1000

    removed old files

--- reader_slots.lua
+++
@@ -1,8 +0,0 @@
--- This table define a round of access to sensor
--- Specify order and times you want access a sensor in a round
-readerSlots = {
-  cfg.readerId.nodeHeap,
-  cfg.readerId.wifiSignal,
-  cfg.readerId.externalTemp, -- This slot read both temp and humidity
-  -- cfg.readerId.otherSensor,
-}

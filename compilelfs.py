551993e83944f8104240dc98f488a6c9b6ac8fa6
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-09-26 15:13:01 +1000

    adding some informational messages to compilelfs.py

--- compilelfs.py
+++ compilelfs.py
@@ -2,7 +2,7 @@
 import os, requests, zipfile, tempfile
 
 t = tempfile.TemporaryFile()
-
+print("Zipping lfs to temp ...")
 def zipdir(path):
     ziph = zipfile.ZipFile(t, 'w', zipfile.ZIP_DEFLATED)
     # ziph is zipfile handle
@@ -12,9 +12,11 @@ def zipdir(path):
 
 zipdir(os.path.join(os.path.dirname(__file__), "lfs"))
 t.seek(0)
-
+print("Zipped.")
+print("Uploading zip to blog.ellisons.org.uk ...")
 resp = requests.post("https://blog.ellisons.org.uk/luac", files={'userfile': ("lfs.zip", t)})
-
+print("{} {}".format(resp.status_code, resp.reason))
 if resp.status_code == 200:
+    print("Success! Writing lfs.img ...")
     with open("lfs.img", 'wb') as f:
         f.write(resp.content)

7050d6aabbb19c12ba0a2b76bc5a881575a7bd8a
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-10-24 09:56:52 +1100

    updated clear.sh

--- clear.sh
+++ clear.sh
@@ -1,3 +1,3 @@
 #/bin/bash
+esptool -b 921600 --port /dev/ttyUSB0 erase_flash
 esptool -b 921600 --port /dev/ttyUSB0 write_flash -fm dio 0x000000 firmware/nodemcu_float_2018-*
-yes| nodemcu-tool mkfs
